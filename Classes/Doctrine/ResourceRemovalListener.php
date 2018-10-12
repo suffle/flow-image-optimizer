61b9d15390d564ff265125c0144421c7b4e69bfa
Karsten Dambekalns <karsten@dambekalns.de>
2018-10-12 17:58:06 +0200

    Avoid endless loop when removing OptimizedResourceRelations

--- Classes/Doctrine/ResourceRemovalListener.php
+++ Classes/Doctrine/ResourceRemovalListener.php
@@ -19,17 +19,27 @@ class ResourceRemovalListener
     protected $optimizedResourceRelationRepository;
 
     /**
+     * @var array
+     */
+    protected $seenOptimizedResourceRelations = [];
+
+    /**
      * @param LifecycleEventArgs $event
      * @throws \Neos\Flow\Persistence\Exception\IllegalObjectTypeException
      */
     public function preRemove(LifecycleEventArgs $event)
     {
-        if (!$event->getEntity() instanceof PersistentResource) {
+        $entity = $event->getEntity();
+        if (!$entity instanceof PersistentResource) {
             return;
         }
 
-        $optimizedResourceRelation = $this->optimizedResourceRelationRepository->findOneByOptimizedResource($event->getEntity());
+        $optimizedResourceRelation = $this->optimizedResourceRelationRepository->findOneByOptimizedResource($entity);
         if ($optimizedResourceRelation instanceof OptimizedResourceRelation) {
+            if (array_key_exists($optimizedResourceRelation->getOriginalResourceIdentificationHash(), $this->seenOptimizedResourceRelations)) {
+                return;
+            }
+            $this->seenOptimizedResourceRelations[$optimizedResourceRelation->getOriginalResourceIdentificationHash()] = true;
             $this->optimizedResourceRelationRepository->remove($optimizedResourceRelation);
         }
     }
